aec5b9227c4bc14291c70fa6b6751d7d01e075be
Evan Dickinson <me@evand.name>
2018-07-31 20:22:03 -0700

    Parse a string

--- src/lib.rs
+++ src/lib.rs
@@ -3,52 +3,103 @@
 pub mod client;
 pub mod network;
 
-pub enum Hat {
-	Bowler,
-	Ball,
-	Fedora,
-	Fancy(u64)
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Square {
+	Unknown,
+	Water,
+	Ship
 }
 
-pub fn hat_cost(hat : &Hat) -> u64 {
-	match *hat {
-		Hat::Bowler => 500,
-		Hat::Ball   => 5,
-		Hat::Fedora => 0,
-		Hat::Fancy(cost) => cost,
+pub fn char_to_square(square_char : char) -> Result<Square, String> {
+	match square_char {
+		' ' => Ok(Square::Unknown),
+		'*' => Ok(Square::Ship),
+		'~' => Ok(Square::Water),
+		_   => Err("Unknown char".to_string())
 	}
 }
 
-pub fn hats_cost(hats: &[Hat]) -> u64 {
-	return hats.iter()
-		.map(hat_cost)
-		.sum();
+pub fn str_to_row(line : &str) -> Result<Vec<Square>, String> {
+	let row : Result<Vec<Square>, String> = line.chars()
+		.map(char_to_square)
+		.collect();
+
+	return row;	
+}
+
+pub fn str_to_rows(text : &str) -> Result<Vec<Vec<Square>>, String> {
+	let rows : Result<Vec<Vec<Square>>, String> = text.split("\n")
+		.map(str_to_row)
+		.collect();
+
+	return rows;		
 }
 
 
-pub fn describe_hat(hat : Hat) -> & 'static str {
-	match hat {
-		Hat::Bowler => "A fine hat",
-		Hat::Ball   => "Bleh",
-		Hat::Fedora => "No!",
-		Hat::Fancy(_)  => "So fancy!"
-	}
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+    #[test]
+    fn it_finds_a_ship() {
+    	let result = char_to_square('*');
+
+        assert_eq!(result, Ok(Square::Ship));
+    }
+
+    #[test]
+    fn it_fails() {
+    	let result = char_to_square('q');
+
+        assert_eq!(result, Err("Unknown char".to_string()));
+    }
+
+    #[test]
+    fn it_makes_a_row() {
+    	let line = "~~* ";
+    	let row = str_to_row(&line);
+
+    	let expected_row = Ok(vec![
+    		Square::Water,
+    		Square::Water,
+    		Square::Ship,
+    		Square::Unknown
+    	]);
+
+    	assert_eq!(row, expected_row);
+    }
+
+    #[test]
+    fn it_fails_to_make_a_row() {
+    	let line = "~q~";
+    	let row = str_to_row(&line);
+
+    	let expected_row = Err("Unknown char".to_string());
+
+    	assert_eq!(row, expected_row);
+    }    
+
+    #[test]
+    fn it_makes_several_rows() {
+    	let text = "~~* \n  *~";
+    	let rows = str_to_rows(text);
+
+    	let expected = Ok(vec![
+    		vec![
+    			Square::Water,
+    			Square::Water,
+    			Square::Ship,
+    			Square::Unknown,    			
+    		],
+    		vec![
+    			Square::Unknown,
+    			Square::Unknown,
+    			Square::Ship,
+    			Square::Water
+    		],
+    	]);
+
+    	assert_eq!(rows, expected);
+    }
 }
-
-pub fn foo() -> Option<i32> {
-	let a = [1, 2, 3, 4];
-	let b = a.iter()
-		.map(|x| { x * 2 })
-		.nth(2);
-		//.unwrap();
-
-	return b;
-}
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
