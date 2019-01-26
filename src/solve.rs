921d93b07535707d2a4fefe85c44c45de35585e0
Evan Dickinson <me@evand.name>
2019-01-26 14:40:44 -0800

    Update solve.rs to rust 2018 style

--- src/solve.rs
+++ src/solve.rs
@@ -1,39 +1,23 @@
 use crate::board::*;
 
 mod fill_unknown;
-use self::fill_unknown::*;
-
 mod surround_with_water;
-use self::surround_with_water::*;
-
 mod specify_ships;
-use self::specify_ships::*;
-
 mod place_at_ends;
-use self::place_at_ends::*;
-
 mod only_place_it_can_go;
-use self::only_place_it_can_go::*;
-
 mod specify_middles;
-use self::specify_middles::*;
-
 mod surround_middles;
-use self::surround_middles::*;
-
-// TODO: Checks to implement:
-// * Count ships (1 x size 3, 2 x size 2, etc.)
 
 pub fn solve(board : &mut Board) {
     let solvers = [
-        fill_with_water,
-        fill_with_ships,
-        surround_ships_with_water,
-        place_ships_next_to_ends,
-        refine_any_ship_to_specific_ship,
-        find_only_place_for_ships,
-        specify_middle,
-        surround_middle_with_ships,
+        self::fill_unknown::fill_with_water,
+        self::fill_unknown::fill_with_ships,
+        self::surround_with_water::surround_ships_with_water,
+        self::place_at_ends::place_ships_next_to_ends,
+        self::specify_ships::refine_any_ship_to_specific_ship,
+        self::only_place_it_can_go::find_only_place_for_ships,
+        self::specify_middles::specify_middle,
+        self::surround_middles::surround_middle_with_ships,
     ];
 
     board.print();
