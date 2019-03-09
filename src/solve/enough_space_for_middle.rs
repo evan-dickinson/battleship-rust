1b229b2ed05679b87c145548b2b67527d408e3e1
Evan Dickinson <me@evand.name>
2019-03-08 22:18:51 -0800

    space for middle: Cleanup

--- src/solve/enough_space_for_middle.rs
+++ src/solve/enough_space_for_middle.rs
@@ -3,50 +3,53 @@ use crate::error::*;
 use crate::neighbor::*;
 use crate::square::*;
 
+
+
 // Determine if an AnyMiddle should be vertical or horizontal by looking at the
 // number of ship squares remaining. 
 pub fn enough_space_for_middle(board: &mut Board) -> Result<()> {
     let layout = board.layout;
 
-    let to_set = layout.all_coordinates()
-        .filter(|coord| board[*coord] == Square::ShipSquare(ShipSquare::AnyMiddle))
-        .filter(|coord| {
-            // Skip squares that are adjacent to board edges, or adjacent to a known square.
-            // Other rules will fill those squares in.
-            let neighbors = [Neighbor::N, Neighbor::E, Neighbor::S, Neighbor::W];
+    for coord in layout.all_coordinates() {
+        if board[coord] != Square::ShipSquare(ShipSquare::AnyMiddle) {
+            continue;
+        }
 
-            neighbors.iter().all(|neighbor| match coord.neighbor(*neighbor) {
+        // Skip squares that are adjacent to board edges, or adjacent to a known square.
+        // Other rules will fill those squares in.
+        let neighbors = [Neighbor::N, Neighbor::E, Neighbor::S, Neighbor::W];
+        let neighbors_ok = neighbors.iter().all(|neighbor| {
+            match coord.neighbor(*neighbor) {
                 Some(neighbor_coord) => board[neighbor_coord] == Square::Unknown,
                 None => false,
-            })
-        })
-        .filter_map(|coord| {
-            let has_space_in_row = board.ship_squares_remaining(coord.row()) >= 2;
-            let has_space_in_col = board.ship_squares_remaining(coord.col()) >= 2;
-
-            match (has_space_in_row, has_space_in_col) {
-                // The row contains enough space for a ship start and ship end,
-                // but the col does not. This must be a horizontal ship.
-                (true,  false) => Some(Ok((coord, ShipSquare::HorizontalMiddle))),
-
-                // Must be a vertical ship
-                (false, true)  => Some(Ok((coord, ShipSquare::VerticalMiddle))),
-
-                // Ship could be either vertical or horizontal. Do nothing.
-                (true,  true)  => None,
-
-                // Error: Neither row nor col has enough space for a ship here
-                (false, false) => {
-                    let message = format!("Ship middle has no space to become vertical or horizontal. {:?}", coord);
-                    Some(Err(message.into()))
-                }
             }
-        })
-        .collect::<Vec<Result<_>>>();
+        });
+        if !neighbors_ok {
+            continue;
+        }
 
-    for item in to_set {
-        let (coord, ship_type) = item?;
-        board.set(coord, Square::ShipSquare(ship_type))?;
+        let has_space_in_row = board.ship_squares_remaining(coord.row()) >= 2;
+        let has_space_in_col = board.ship_squares_remaining(coord.col()) >= 2;
+        let ship_type_opt = match (has_space_in_row, has_space_in_col) {
+            // The row contains enough space for a ship start and ship end,
+            // but the col does not. This must be a horizontal ship.
+            (true,  false) => Some(ShipSquare::HorizontalMiddle),
+
+            // Must be a vertical ship
+            (false, true)  => Some(ShipSquare::VerticalMiddle),
+
+            // Ship could be either vertical or horizontal. Do nothing.
+            (true,  true)  => None,
+
+            // Error: Neither row nor col has enough space for a ship here
+            (false, false) => {
+                bail!("Ship middle has no space to become vertical or horizontal. {:?}", coord)
+            }
+        };
+
+        if let Some(ship_type) = ship_type_opt {
+            board.set(coord, Square::ShipSquare(ship_type))?;
+        }
     }
 
     Ok(())
